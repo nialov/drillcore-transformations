a335bd48fb8131f052aa6021b0264c47299dc92b
nialov <nikolasovaskainen@gmail.com>
2022-09-02 12:47:27 +0300

    docs: update version

--- drillcore_transformations/__init__.py
+++ drillcore_transformations/__init__.py
@@ -6,4 +6,4 @@ Transform structural drillcore measurements.
 
 __author__ = """Nikolas Ovaskainen"""
 __email__ = "nikolasovaskainen@gmail.com"
-__version__ = "0.2.6.post3.dev0+66a4458"
+__version__ = "0.2.6.post9.dev0+a7d935e"
