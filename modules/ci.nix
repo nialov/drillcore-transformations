c95b1fc055eb88d1797ab1ae2b26db12c49baca2
nialov <nikolasovaskainen@gmail.com>
2025-10-17 13:45:08 +0300

    ci: refactor CI configuration for improved readability and structure

--- modules/ci.nix
+++ modules/ci.nix
@@ -24,46 +24,44 @@ in
   imports = [
     inputs.actions-nix.flakeModules.default
   ];
-  flake.actions-nix =
-    { config, ... }:
-    {
-      pre-commit.enable = true;
-      defaults = {
-        jobs = {
-          timeout-minutes = 60;
-          runs-on = "ubuntu-latest";
-        };
+  flake.actions-nix = {
+    pre-commit.enable = true;
+    defaults = {
+      jobs = {
+        timeout-minutes = 60;
+        runs-on = "ubuntu-latest";
       };
-      workflows = {
-        ".github/workflows/main.yaml" = {
-          on = {
-            push = { };
-            workflow_dispatch = { };
-            pull_request = { };
+    };
+    workflows = {
+      ".github/workflows/main.yaml" = {
+        on = {
+          push = { };
+          workflow_dispatch = { };
+          pull_request = { };
+        };
+        jobs = {
+          nix-flake-check = {
+            steps = baseNixSteps ++ [
+              runNixFlakeCheck
+            ];
           };
-          jobs = {
-            nix-flake-check = {
-              steps = baseNixSteps ++ [
-                runNixFlakeCheck
-              ];
-            };
-            # uv-pytest = {
-            #   steps = baseNixSteps ++ [
-            #     {
-            #       run = ''
-            #         nix run .#fhs -- -c 'uv sync --all-extras'
-            #       '';
-            #     }
-            #     {
-            #       run = ''
-            #         nix run .#fhs -- -c 'uv run pytest -v'
-            #       '';
-            #     }
+          # uv-pytest = {
+          #   steps = baseNixSteps ++ [
+          #     {
+          #       run = ''
+          #         nix run .#fhs -- -c 'uv sync --all-extras'
+          #       '';
+          #     }
+          #     {
+          #       run = ''
+          #         nix run .#fhs -- -c 'uv run pytest -v'
+          #       '';
+          #     }
 
-            #   ];
-            # };
-          };
+          #   ];
+          # };
         };
       };
     };
+  };
 }
