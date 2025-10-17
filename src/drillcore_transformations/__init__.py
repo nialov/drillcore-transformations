2b7c1466dc39996966c438a6db1e4b2dfe5d91a7
nialov <nikolasovaskainen@gmail.com>
2025-10-17 13:57:20 +0300

    build: update version

--- src/drillcore_transformations/__init__.py
+++ src/drillcore_transformations/__init__.py
@@ -6,4 +6,4 @@ Transform structural drillcore measurements.
 
 __author__ = """Nikolas Ovaskainen"""
 __email__ = "nikolasovaskainen@gmail.com"
-__version__ = "0.2.6.post9.dev0+a7d935e"
+__version__ = "0.3.0"
